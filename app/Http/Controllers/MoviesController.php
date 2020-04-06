e5e0d3de4f88979953e67f8384b9c00680d78bc6
Burhanudin Yahya <bor.yahya@gmail.com>
2020-04-07 00:21:29 +0700

    update MoviesController for reusable

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -2,70 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class MoviesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        $popularMovies = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/movie/popular')
-            ->json()['results'];
-
-        $genresArray = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json()['genres'];
-
-        $genres = collect($genresArray)->mapWithKeys(function ($genre) {
-            return [$genre['id'] => $genre['name']];
-        });
-
-        $nowPlayingMovies = Http::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/movie/now_playing')
-            ->json()['results'];
-
-        return view('index', [
-            'title' => config ('app.name'),
-            'metaDescription' => 'Mufiap adalah aplikasi penyedia list movie terlengkap dan terupdate di dunia.',
-            'popularMovies' => $popularMovies,
-            'genres' => $genres,
-            'nowPlayingMovies' => $nowPlayingMovies
-        ]);
+        return self::showList ('popular');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function popular()
     {
-        //
+        return self::showList ('popular');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function nowPlaying()
     {
-        //
+        return self::showList ('now_playing');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    public function upcoming()
+    {
+        return self::showList ('upcoming');
+    }
+
+    public function topRated()
+    {
+        return self::showList ('top_rated');
+    }
+
     public function show($id)
     {
         $movie = Http::withToken(config('services.tmdb.token'))
@@ -79,37 +45,35 @@ class MoviesController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function showList($section)
     {
-        //
+        $ucWord =  ucwords( str_replace ('_', ' ', $section) );
+        return view('movie', [
+            'title' => $ucWord . ' Movies - ' . config ('app.name'),
+            'metaDescription' => 'Mufiap adalah aplikasi penyedia list movie terlengkap dan terupdate di dunia.',
+            'section' => $ucWord . ' Movies',
+            'movies' => self::list ($section),
+            'genres' => self::genres ()
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public static function list($section)
     {
-        //
+        return Http::withToken(config('services.tmdb.token'))
+                   ->get('https://api.themoviedb.org/3/movie/' . $section)
+                   ->json()['results'];
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public static function genres()
     {
-        //
+        $genresArray = Http::withToken(config('services.tmdb.token'))
+                           ->get('https://api.themoviedb.org/3/genre/movie/list')
+                           ->json()['genres'];
+
+        return collect($genresArray)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
+
     }
+
 }
