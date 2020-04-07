1e9adc7d38cf40c5b6ed63f280fd738aec8460b4
Burhanudin Yahya <bor.yahya@gmail.com>
2020-04-07 08:46:33 +0700

    grid default two col

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
             <a href="{{ route('movies.popular') }}">
                 <h2 class="px-4 uppercase tracking-wider text-orange-500 text-lg font-semibold hover:text-orange-900 inline">Popular</h2>
             </a>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+            <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
                 @foreach ($popularMovies as $movie)
                     @if ($loop->index < 5)
                         <x-movie-card :movie='$movie' :genres='$genres' />
@@ -19,7 +19,7 @@
             <a href="{{ route('movies.now_playing') }}">
                 <h2 class="px-4 uppercase tracking-wider text-orange-500 text-lg font-semibold hover:text-orange-900 inline">Now Playing</h2>
             </a>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+            <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
                 @foreach ($nowPlayingMovies as $movie)
                     @if ($loop->index < 5)
                         <x-movie-card :movie='$movie' :genres='$genres' />
@@ -32,7 +32,7 @@
             <a href="{{ route('movies.upcoming') }}">
                 <h2 class="px-4 uppercase tracking-wider text-orange-500 text-lg font-semibold hover:text-orange-900 inline">Upcoming</h2>
             </a>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+            <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
                 @foreach ($upcomingMovies as $movie)
                     @if ($loop->index < 5)
                         <x-movie-card :movie='$movie' :genres='$genres' />
@@ -45,7 +45,7 @@
             <a href="{{ route('movies.top_rated') }}">
                 <h2 class="px-4 uppercase tracking-wider text-orange-500 text-lg font-semibold hover:text-orange-900 inline">Top Rated</h2>
             </a>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+            <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
                 @foreach ($topRatedMovies as $movie)
                     @if ($loop->index < 5)
                         <x-movie-card :movie='$movie' :genres='$genres' />
