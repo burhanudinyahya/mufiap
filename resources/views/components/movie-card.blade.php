550c921d9498cd702caae3c735db7e2daf715d73
Burhanudin Yahya <bor.yahya@gmail.com>
2020-04-08 07:16:01 +0700

    handle image not found in movie card component

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-8 px-4 py-4 hover:shadow-md">
     <a href="{{ route('movies.show', $movie['id']) }}">
-        <img src="https://image.tmdb.org/t/p/w300/{{ $movie['poster_path'] }}" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150">
+        <img src="https://image.tmdb.org/t/p/w300/{{ $movie['poster_path'] }}" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150" onerror="this.onerror=null;this.src='https://via.placeholder.com/300x450?text=IMAGE+NOT+AVAILABLE';">
     </a>
     <div class="mt-2">
         <a href="{{ route('movies.show', $movie['id']) }}" class="text-lg mt-2 hover:text-black:300">{{ $movie['title'] }}</a>
